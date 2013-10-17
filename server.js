5d94d8962458b68098d3968b91fa4cf5d6cd8156
Creg Bradley <cbradley@daptiv.com>
2013-10-17 16:53:16 -0700

    Added support for tables endpoint

---
+++ server.js
@@ -0,0 +1,22 @@
+/* jshint node: true */
+'use strict';
+var restify = require('restify'),
+	tables = require('./tables')	
+
+
+var server = restify.createServer(),
+    port = process.env.PORT || 8081;
+
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+}
+
+//	server.use(restify.bodyParser({ mapParams: true }));
+
+server.get('/hello/:name', respond);
+tables.register(server);
+
+
+server.listen(port);
+
+console.log('> echo server is listening on http://127.0.0.1:'+port);
