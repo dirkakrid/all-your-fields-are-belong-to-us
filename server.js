84d4fd6cc15dba631590b42779985e916fb8fb78
Creg Bradley <cbradley@daptiv.com>
2013-10-17 13:22:47 -0700

    Barebones restify server

---
+++ server.js
@@ -0,0 +1,18 @@
+/* jshint node: true */
+'use strict';
+var restify = require('restify'),
+    mongoose = require('mongoose')
+
+var server = restify.createServer(),
+    port = process.env.PORT || 8081;
+
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+}
+
+//	server.use(restify.bodyParser({ mapParams: true }));
+
+server.get('/hello/:name', respond);
+server.listen(port);
+
+console.log('> echo server is listening on http://127.0.0.1:'+port);
